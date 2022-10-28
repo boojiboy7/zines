83d1da70d43f20daf384389797c1b1a5623d2af1
boojiboy7 <69740328+boojiboy7@users.noreply.github.com>
2022-10-28 10:43:32 -0400

    Update ezmreader.js

--- 11_speccy/ezmreader.js
+++ 11_speccy/ezmreader.js
@@ -42,7 +42,7 @@ const TEMPLATE = 4; // Change this value to set the template
     8: 64 pages
         Micro
 */
-const BGCOLOR = '#81f777'; // Change this hex value to set the background color. Remember to keep the quotes!
+const BGCOLOR = '#7dfc72'; // Change this hex value to set the background color. Remember to keep the quotes!
 const ALT = 'Eleven ZX Spectrum Games of Interest'; // Change this to a plaintext copy or description of your content to make it visible to screen-readers
 const SMOOTH = true; // Set to false if you want crispy pixels. Leave true if you like the blur.
 
