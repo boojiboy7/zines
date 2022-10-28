4095e6b5e26de42f38635c0d9ce8e56b41898c3c
boojiboy7 <69740328+boojiboy7@users.noreply.github.com>
2022-10-27 20:58:41 -0400

    Update ezmreader.js

--- practical_cat_summoning/ezmreader.js
+++ practical_cat_summoning/ezmreader.js
@@ -42,8 +42,8 @@ const TEMPLATE = 1; // Change this value to set the template
     8: 64 pages
         Micro
 */
-const BGCOLOR = '#f5f5f5'; // Change this hex value to set the background color. Remember to keep the quotes!
-const ALT = 'Reader for Electric Zine Maker'; // Change this to a plaintext copy or description of your content to make it visible to screen-readers
+const BGCOLOR = '#000000'; // Change this hex value to set the background color. Remember to keep the quotes!
+const ALT = 'Practical Cat Summoning'; // Change this to a plaintext copy or description of your content to make it visible to screen-readers
 const SMOOTH = true; // Set to false if you want crispy pixels. Leave true if you like the blur.
 
 //---- END USER OPTIONS ----//
